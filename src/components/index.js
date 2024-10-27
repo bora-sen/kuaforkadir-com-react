f856d91ef18238cc98901bd976e1b017886cd728
bora-sen <bahattinborasen@gmail.com>
2024-10-27 21:42:26 +0300

    Added index file to components folder

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Contact } from "./Contact"
+export { default as Footer } from "./Footer"
+export { default as GoogleMaps } from "./GoogleMaps"
+export { default as Header } from "./Header"
+export { default as Hero } from "./Hero"
+export { default as Services } from "./Services"
+export { default as WorkingHours } from "./WorkingHours"
