10be9ff486babb02ca118cf4d9dcdb77f353e38e
bora-sen <bahattinborasen@gmail.com>
2024-11-02 22:43:09 +0300

    Add full-width background && remove background color to Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,34 +3,36 @@ import * as Icons from "../assets/icons"
 
 function Contact() {
   return (
-    <div className="container contact-margin">
-      <div className="row">
-        <div className="col d-flex justify-content-center align-items-center">
-          <div className="d-flex">
-            <img width="60px" src={Icons.Map} alt="" />
-            <div className="d-flex flex-column justify-content-center mx-4 gap-2">
-              <span className="fw-bold font-display">ADRES</span>
-              <span>790 FOLSOM AVE, SAN FRANCISCO</span>
+    <div className="container-fluid contact-margin bg-secondary-subtle py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center">
+            <div className="d-flex">
+              <img width="60px" src={Icons.Map} alt="" />
+              <div className="d-flex flex-column justify-content-center mx-4 gap-2">
+                <span className="fw-bold font-display">ADRES</span>
+                <span>790 FOLSOM AVE, SAN FRANCISCO</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col d-flex justify-content-center align-items-center">
-          <div className="d-flex">
-            <img width="60px" src={Icons.Email} alt="" />
-            <div className="d-flex flex-column justify-content-center mx-4 gap-2">
-              <span className="fw-bold font-display">E-POSTA</span>
-              <span className="text-uppercase">place@placeholder.com</span>
+          <div className="col d-flex justify-content-center align-items-center">
+            <div className="d-flex">
+              <img width="60px" src={Icons.Email} alt="" />
+              <div className="d-flex flex-column justify-content-center mx-4 gap-2">
+                <span className="fw-bold font-display">E-POSTA</span>
+                <span className="text-uppercase">place@placeholder.com</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="col d-flex justify-content-center align-items-center">
-          <div className="d-flex">
-            <img width="60px" src={Icons.Phone} alt="" />
-            <div className="d-flex flex-column justify-content-center mx-4 gap-2">
-              <span className="fw-bold font-display">TELEFON</span>
-              <span>+90 555 555 55 55</span>
+          <div className="col d-flex justify-content-center align-items-center">
+            <div className="d-flex">
+              <img width="60px" src={Icons.Phone} alt="" />
+              <div className="d-flex flex-column justify-content-center mx-4 gap-2">
+                <span className="fw-bold font-display">TELEFON</span>
+                <span>+90 555 555 55 55</span>
+              </div>
             </div>
           </div>
         </div>
