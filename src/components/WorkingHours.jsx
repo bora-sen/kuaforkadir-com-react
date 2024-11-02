755ce89cee4eda07788aa38f631905c5c871a0d7
bora-sen <bahattinborasen@gmail.com>
2024-11-02 22:44:03 +0300

    Update new title && change working hours list ui

--- src/components/WorkingHours.jsx
+++ src/components/WorkingHours.jsx
@@ -3,15 +3,14 @@ import React from "react"
 function WorkingHours() {
   const Days = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
   return (
-    <div className="container bg-info px-5">
-      <div className="row text-center mb-4">
-        <span className="fs-2 font-display">MAIN TITLE</span>
-        <span className="fs-4">Subtitle</span>
+    <div className="container px-5">
+      <div className="row text-center mt-5">
+        <span className="fs-2 font-display">Çalışma Saatlerimiz</span>
       </div>
 
-      <div className="row bg-success p-5 w-50 mx-auto">
+      <div className="row p-5 w-50 mx-auto">
         {Days.map((day, index) => (
-          <div key={index} className="py-4 border border-dark working-hour-item my-2">
+          <div key={index} className="py-4 border-bottom border-dark-subtle working-hour-item mb-2">
             <div className="d-flex justify-content-between align-items-center w-100">
               <span>{day}</span>
               <span>{(day == "Pazar") ? "Kapalı" : "08:00 - 20:00"}</span>
