9e03779ea890b6461d3b9d47c40cd49a00fbe86c
bora-sen <bahattinborasen@gmail.com>
2024-11-04 21:18:08 +0300

    Add animate.css to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./scss/styles.scss"
 import App from './App.jsx'
+import "animate.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
