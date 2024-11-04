a17d4f4566043c8f3893dd1030b038e82e1596f0
bora-sen <bahattinborasen@gmail.com>
2024-11-04 21:17:04 +0300

    Refactor svg's attribute for react.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,9 +12,9 @@ function Footer() {
               width={IconWidth}
               fill="none"
               stroke="black"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -32,9 +32,9 @@ function Footer() {
               width={IconWidth}
               stroke="black"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -49,15 +49,15 @@ function Footer() {
               width={IconWidth}
               stroke="black"
               xmlns="http://www.w3.org/2000/svg"
-              shape-rendering="geometricPrecision"
-              text-rendering="geometricPrecision"
-              image-rendering="optimizeQuality"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              shapeRendering="geometricPrecision"
+              textRendering="geometricPrecision"
+              imageRendering="optimizeQuality"
+              fillRule="evenodd"
+              clipRule="evenodd"
               viewBox="0 0 512 462.799"
             >
               <path
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
               />
             </svg>
