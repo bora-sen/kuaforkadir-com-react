198ad8b882fc477a63fb6b245fa3e4e74102b888
bora-sen <bahattinborasen@gmail.com>
2024-11-07 13:49:28 +0300

    Add Collapsing Navbar to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,37 @@ import React from "react"
 
 function Header() {
   return (
-    <header className="bg-black text-white position-fixed top-0 container-fluid">
+    <header className="z-1 bg-black text-white position-fixed top-0 container-fluid header-padding">
       <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container-fluid">
-          <a className="mx-4 navbar-brand" href="#">
-            Berber
+          <a className="mx-4 navbar-brand font-hero fs-1" href="#">
+            KuaforKadir
           </a>
-        </div>
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Test1
-            </a>
-          </li>
+        <button
+          className="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle Navbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Test1
-            </a>
-          </li>
-        </ul>
+        <div id="navbarNav" className="collapse navbar-collapse text-center">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#Servislerimiz">
+                Servislerimiz
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link text-nowrap" href="#BizeUlasin">
+                Bize Ulaşın
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
