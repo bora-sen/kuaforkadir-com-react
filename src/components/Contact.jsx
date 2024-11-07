f5d995c88f83e3e680d33a7063c8f64463e2bffa
bora-sen <bahattinborasen@gmail.com>
2024-11-07 16:33:42 +0300

    Add Environment variables to sensitive data

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import * as Icons from "../assets/icons"
+const address = import.meta.env.VITE_ADDRESS
+const mail = import.meta.env.VITE_MAIL
+const phone = import.meta.env.VITE_PHONE_NUMBER
 
 function Contact() {
   return (
@@ -11,7 +14,7 @@ function Contact() {
               <img width="60px" src={Icons.Map} alt="" />
               <div className="d-flex flex-column justify-content-center mx-4 gap-2">
                 <span className="fw-bold font-display">ADRES</span>
-                <span>790 FOLSOM AVE, SAN FRANCISCO</span>
+                <span>{address}</span>
               </div>
             </div>
           </div>
@@ -21,7 +24,7 @@ function Contact() {
               <img width="60px" src={Icons.Email} alt="" />
               <div className="d-flex flex-column justify-content-center mx-4 gap-2">
                 <span className="fw-bold font-display">E-POSTA</span>
-                <span className="text-uppercase">place@placeholder.com</span>
+                <span className="text-uppercase">{mail}</span>
               </div>
             </div>
           </div>
@@ -31,7 +34,7 @@ function Contact() {
               <img width="60px" src={Icons.Phone} alt="" />
               <div className="d-flex flex-column justify-content-center mx-4 gap-2">
                 <span className="fw-bold font-display">TELEFON</span>
-                <span>+90 555 555 55 55</span>
+                <span>{phone}</span>
               </div>
             </div>
           </div>
